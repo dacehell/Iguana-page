07363e7a801a8ff5d6ce614df527529cebaa7ecf
David Caldera <caldera.david78@gmail.com>
2022-11-25 21:04:32 -0400

    ricomida

---
+++ ricomida/assets/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('[data-bs-toggle="tooltip"]').tooltip();
+
+  $("#enviar").click(function () {
+    alert("El correo fue enviado.....");
+  });
+
+  $(".underline").dblclick(function () {
+    $(this).css({
+      color: "red !important",
+    });
+  });
+});
